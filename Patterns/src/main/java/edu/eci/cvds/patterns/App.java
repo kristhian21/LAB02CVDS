646d1354d3cbd45d9713346292efe8396b3cfc24
MariaJoseTorresN <majito.200130@gmail.com>
2021-08-22 21:40:22 -0500

    hello world y otros

--- Patterns/src/main/java/edu/eci/cvds/patterns/App.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -1,5 +1,5 @@
 package edu.eci.cvds.patterns;
-
+import java.util.Arrays;
 /**
  * Hello world!
  *
@@ -7,8 +7,11 @@ package edu.eci.cvds.patterns;
 public class App 
 {
     public static void main( String[] args ) {
-
-        System.out.println("Hello " + args[0] + "!");
-
+        if(args.length == 0){
+            System.out.println("Hello world!");
+        }
+        else{
+            System.out.println("Hello " + Arrays.toString(args).replace("[", "").replace("]", "").replace(",", " ") + "!");
+        }
     }
 }
